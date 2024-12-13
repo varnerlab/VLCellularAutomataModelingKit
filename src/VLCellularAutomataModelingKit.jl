2f8fbf51bdf80c9124c90db30f890daec9efe126
Jeffrey Varner <jdv27@cornell.edu>
2024-12-13 15:50:37 -0500

    test
    test

--- src/VLCellularAutomataModelingKit.jl
+++ src/VLCellularAutomataModelingKit.jl
@@ -3,4 +3,10 @@ module VLCelluarAutomataModelingKit
     # include -
     include("Include.jl")
 
+    # export -
+    export AbstractPolicyModel, AbstractLearningModel, AbstractWorldModel, AbstractAgentModel
+    export MyElementaryWolframRuleModel, MyTotalisticWolframRuleModel, MySimpleAgentModel
+    export build
+
+
 end
