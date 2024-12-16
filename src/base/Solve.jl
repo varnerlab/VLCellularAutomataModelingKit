2e5be62c31aa8ff7a6e1ce317a3f878b013374f5
Jeffrey Varner <jdv27@cornell.edu>
2024-12-16 11:00:56 -0500

    Update Solve.jl
    ok this should work

--- src/base/Solve.jl
+++ src/base/Solve.jl
@@ -129,8 +129,7 @@ function _solve(agents::Array{MySimpleTwoDimensionalAgentModel,1}, world::MyTwoD
             for col ∈ (2:width-1)
 
                 coordinate = (row, col);
-                if (true)
-                    @show ∈(coordinate, exclude) == false
+                if (∈(coordinate, exclude) == false)
                     agent_index = coordinates[coordinate];
                     next_state = _execute(agents[agent_index], current_frame, world);
                     next_frame[row, col] = next_state;
